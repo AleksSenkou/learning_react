10549476b957337123236c56e674db9118ea23c8
alekssenkou <aleksey.senkou@gmail.com>
2016-07-29 13:28:31 +0300

    Add to the header a link to a Home page

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export default React.createClass({
   render() {
     return (
       <div className='main-container'>
+        <div>
+          <Link to='/'>
+            <button className='btn btn-default'> Home </button>
+          </Link>
+        </div>
         {this.props.children}
       </div>
     )
