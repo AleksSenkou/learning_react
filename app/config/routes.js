b8f6c0aa23776809b11945d66519bc86c0244195
alekssenkou <aleksey.senkou@gmail.com>
2016-07-29 13:29:07 +0300

    Add playerTwo page

--- app/config/routes.js
+++ app/config/routes.js
@@ -9,6 +9,7 @@ let routes = (
     <Route path='/' component={Main}>
       <IndexRoute component={Home}/>
       <Route path='playerOne' header='Player One' component={PromptContainer}/>
+      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer}/>
     </Route>
   </Router>
 )
--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import styles from '../styles'
 
-class PromptContainer extends Component {
+export default class PromptContainer extends Component {
   constructor(props, context) {
     super(props, context)
 
     this.state = {
       username: ''
     }
-  }
+  };
 
   onUpdateUser = (e) => {
-    this.setState({
-      username: e.target.value
-    })
-  }
+    this.setState({ username: e.target.value })
+  };
+
+  onSubmitUser = (e) => {
+    e.preventDefault()
+
+    // let username = this.state.username
+    this.setState({ username: '' })
+
+    if (this.props.routeParams.playerOne) {
+      this.context.router.push({
+        pathname: '/ballte',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+    } else {
+      this.context.router.push(`/playerTwo/${this.state.username}`)
+    }
+  };
 
   render() {
     return (
@@ -23,7 +40,7 @@ class PromptContainer extends Component {
         style={styles.transparentBg}>
         <h1> {this.props.route.header} </h1>
         <div className='col-sm-12'>
-          <form>
+          <form onSubmit={this.onSubmitUser}>
             <div className='form-group'>
               <input
                 className='form-control'
@@ -43,7 +60,9 @@ class PromptContainer extends Component {
         </div>
       </div>
     )
-  }
+  };
 }
 
-export default PromptContainer
+PromptContainer.contextTypes = {
+  router: PropTypes.object.isRequired
+};
